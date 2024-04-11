7471ea89fbd2cdd904ce76f7be05c13418c7b6d9
brad@smithwebtek.com <brad@smithwebtek.com>
2024-04-10 20:28:17 -0400

    Add root index, add cancel

--- src/routes/destroy.jsx
+++ src/routes/destroy.jsx
@@ -2,7 +2,6 @@ import {redirect} from "react-router-dom";
 import {deleteContact} from "../contacts";
 
 export async function action({params}) {
-  throw new Error("Oh dang!");
   await deleteContact(params.contactId);
   return redirect("/");
 }
