913cdfd2653783cafbf3abdf5cb2cbb49f37d64e
brad@smithwebtek.com <brad@smithwebtek.com>
2024-04-11 21:18:55 -0400

    Search spinner and favorite star

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import "./index.css";
 import Root, {loader as rootLoader, action as rootAction} from "./routes/root";
 import ErrorPage from "./error-page";
-import Contact, {loader as contactLoader} from "./routes/contact";
+import Contact, {loader as contactLoader, action as contactAction} from "./routes/contact";
 import EditContact, {action as editAction} from "./routes/edit";
 import Index from "./routes/index";
 import {action as destroyAction} from "./routes/destroy";
@@ -25,6 +25,7 @@ const router = createBrowserRouter([
         path: "contacts/:contactId",
         element: <Contact />,
         loader: contactLoader,
+        action: contactAction,
       },
       {
         path: "contacts/:contactId/edit",
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -7,7 +7,7 @@ import {
   redirect,
   NavLink,
   useNavigation,
-  useSubmit
+  useSubmit,
 } from "react-router-dom";
 import {getContacts, createContact} from "../contacts";
 
@@ -32,6 +32,10 @@ export default function Root() {
   const navigation = useNavigation();
   const submit = useSubmit();
 
+  const searching =
+    navigation.location &&
+    new URLSearchParams(navigation.location.search).has("q");
+
   useEffect(() => {
     document.getElementById("q").value = q;
   }, [q]);
@@ -47,16 +51,20 @@ export default function Root() {
           <form id="search-form" role="search">
             <input
               id="q"
+              className={searching ? "loading" : ""}
               aria-label="Search contacts"
               placeholder="Search"
               type="search"
               name="q"
               defaultValue={q}
               onChange={(event) => {
-                submit(event.currentTarget.form)
+                const isFirstSearch = q == null;
+                submit(event.currentTarget.form, {
+                  replace: !isFirstSearch,
+                });
               }}
             />
-            <div id="search-spinner" aria-hidden hidden={true} />
+            <div id="search-spinner" aria-hidden hidden={!searching} />
             <div className="sr-only" aria-live="polite"></div>
           </form>
           <Form method="post">
